36bea6d989f1927659c6c0a9910e3bcc43721aef
nkmishra217 <deeksha.mishra_cs18@gla.ac.in>
2019-09-21 17:24:21 +0530

    add getter

---
+++ src/book/Book.java
@@ -0,0 +1,43 @@
+package book;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private String isbn;
+
+    Book(String name, String author, String isbn) {
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "------------------------------\nBook Name:\t" + getName() + "\nAuthor Name:\t" + getAuthor() + "\nISBN:\t" + getIsbn() + "\n------------------------------";
+    }
+}
---
+++ src/book/Solution.java
@@ -0,0 +1,16 @@
+package book;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Book[] book = new Book[n];
+        if (n == 0) {
+            System.out.println("N/A");
+        } else {
+            scanner.nextLine();
+        }
+    }
+}
